70beb663b9b79fb8c45553ccc45a6136ddbe1fa6
Oleg <Stamin169@yandex.ru>
2024-11-13 22:31:06 +0300

    добавление карточек и кнопки удаления

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+
+const placesList = document.querySelector('.places__list');
+
+function addCards(link, name) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const templateElement = cardTemplate.querySelector('.places__item ').cloneNode(true);
+  
+  templateElement.querySelector('.card__image').src = link;
+  templateElement.querySelector('.card__title').textContent = name;
+  templateElement.querySelector('.card__delete-button').addEventListener('click', function () {
+    deleteCard(templateElement);
+});
+
+  return templateElement;
+
+}
+
+const renderCard = function () {
+  initialCards.forEach(function (card) {
+    placesList.append(addCards(card.link, card.name));
+  });
+}
+renderCard()
+
+function deleteCard(placesList) {
+  placesList.remove()
+
+}
